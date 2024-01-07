082c5e97895fbbfa1f62ecd94e32700555f38ef3
aidenj1808 <aidenj1808@gmail.com>
2024-01-07 14:59:59 -0700

    Added my solution for day 4.

---
+++ 2023/Python/day4.py
@@ -0,0 +1,26 @@
+file = open("in.txt")
+part1 = 0
+part2 = []
+for i, line in enumerate(file):
+    all_numbers = line.strip().split(":")[1].strip().split(" | ")
+    numbers_have, winning_numbers = [numbers.split() for numbers in all_numbers]
+    points = 0
+    winners = 0
+    for number_have in numbers_have:
+        if number_have in winning_numbers:
+            winners += 1
+            if points == 0:
+                points += 1
+            else:
+                points *= 2
+    part1 += points
+    part2.append([i + 1, winners])
+
+count = len(part2)
+for game, winners in part2:
+    for i in range(1, winners + 1):
+        if game + i <= count:
+            part2.append([game + i, part2[game + i - 1][1]])
+file.close()
+print(f"The answer to part 1 is: {part1}")
+print(f"The answer to part 2 is: {len(part2)}")
