e7a6a8ac47e28a91afb66eff50fa0596a25b9423
aidenj1808 <aidenj1808@gmail.com>
2024-01-07 16:54:27 -0700

    Added my solution for day 8 part 1.

---
+++ 2023/Python/day8.py
@@ -0,0 +1,21 @@
+file = open("in.txt")
+directions = file.readline().strip()
+file.readline()
+my_dict = {}
+for line in file:
+    p1 = line.strip().split(" = ")[0]
+    p2, p3 = line.strip(")\n").split("(")[1].split(", ")
+    my_dict.update({p1 : (p2, p3)})
+
+def solution(current):
+    steps = 0
+    while True:
+        for direction in directions:
+            if current == "ZZZ":
+                return steps
+            if direction == 'L':
+                current = my_dict[current][0]
+            elif direction == 'R':
+                current = my_dict[current][1]
+            steps += 1
+print(f"The answer for part 1 is: {solution("AAA")}")
