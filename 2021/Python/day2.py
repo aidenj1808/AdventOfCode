7ff430954ef4d0f35e9769b1bc6861182ff3bb5f
aidenj1808 <aidenj1808@gmail.com>
2024-11-30 21:03:15 -0700

    Add day 2 solution.

---
+++ 2021/Python/day2.py
@@ -0,0 +1,18 @@
+with open("inputs/in2.txt") as file:
+    x, y = 0, 0
+    y2, aim = 0, 0,
+    for line in file:
+        line = line.strip().split(" ")
+        if line[0] == "forward":
+            x += int(line[1])
+            y2 += int(line[1]) * aim
+        elif line[0] == "down":
+            y += int(line[1])
+            aim += int(line[1])
+        elif line[0] == "up":
+            y -= int(line[1])
+            aim -= int(line[1])
+    part1 = x * y
+    part2 = x * y2
+print(part1)
+print(part2)
